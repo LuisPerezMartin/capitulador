7390dd0c6fe4046df5866c7d9d861d62de44f09a
Luis Pérez Martín <personal@luisperezmartin.com>
2025-07-23 11:55:20 +0200

    refactor: rename gui

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ import subprocess
 from pathlib import Path
 
 from capitulador import Capitulador
-from config.config import settings, BookSettings
+from config.config import BookSettings
 
 
 class CapituladorGUI:
